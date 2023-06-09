5ccc46cac264a751b4e46a46d3a30dce184f852c
mmigo1 <75628415+mmigo1@users.noreply.github.com>
2023-06-09 09:57:18 +0300

    Final 1.0

--- service/core/urls.py
+++ service/core/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from rest_framework import routers
 
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('api/', question_api, name='api'),
+    path('question', question_api, name='api'),
 ]
--- service/core/views.py
+++ service/core/views.py
@@ -3,25 +3,21 @@ from rest_framework import status
 from rest_framework.response import Response
 import requests
 
-from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework.decorators import api_view
-from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.request import Request
+
 from core.models import Question
 from core.serializers import QuestionSerializer
 
-from django.http import HttpResponse
-
 
 def request_quest(number: int):
-    url = f'https://jservice.io/api/random?count={number}'
+    url: str = f'https://jservice.io/api/random?count={number}'
     response = requests.get(url)
     return response.json()
 
 
 def add_question(questions_number: int):
-    for x in request_quest(questions_number):
-        id: int = x['id']
+    for quest in request_quest(questions_number):
+        id: int = quest['id']
         try:
             question_obj: object = Question.objects.get(pk=id)
         except ObjectDoesNotExist:
@@ -29,30 +25,13 @@ def add_question(questions_number: int):
         if question_obj:
             add_question(1)
             continue
-        question: str = x['question']
-        answer: str = x['answer']
-        created_at = x['created_at']
+        question: str = quest['question']
+        answer: str = quest['answer']
+        created_at = quest['created_at']
         serializer = QuestionSerializer(
-            data= {'id': id, 'question': question, "answer": answer, 'created_at': created_at})
+            data={'id': id, 'question': question, "answer": answer, 'created_at': created_at})
         if serializer.is_valid():
             serializer.save()
-
-
-@api_view((['GET', 'POST']))
-def index(request):
-    if request.method == 'POST':
-        questions_number: int = request.POST.get("questions_num", 'Undefined')
-        add_question(questions_number)
-        try:
-            return render(request=request, template_name='core/index.html',
-                          context={'question': Question.objects.latest("data_uploaded")})
-        except:
-            return render(request=request, template_name='core/index.html')
-    try:
-        return render(request=request, template_name='core/index.html',
-                      context={'question': Question.objects.latest("data_uploaded")})
-    except:
-        return render(request=request, template_name='core/index.html')
 
 
 @api_view((['GET', 'POST']))
@@ -61,17 +40,20 @@ def question_api(request):
         try:
             question_last: object = Question.objects.latest("data_uploaded")
             serializer: object = QuestionSerializer(question_last, many=False)
-            return Response({'Последний сохранённый вопрос викторины': serializer.data})
+            return Response({'Последний сохранённый вопрос викторины': serializer.data}, status=status.HTTP_200_OK)
         except:
-            return Response({})
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
 
     elif request.method == 'POST':
         try:
+            if type(question_num=request.data['questions_num']) is not 'int':
+                return Response(['Введён неверный тип данных'], status=status.HTTP_400_BAD_REQUEST)
             questions_number: int = request.data['questions_num']
         except:
-            return Response(['Введён неверный тип данных'],status=status.HTTP_400_BAD_REQUEST)
-        questions_number: int = request.data['questions_num']
+            return Response(['Введён неверный тип данных'], status=status.HTTP_400_BAD_REQUEST)
         add_question(questions_number)
         question_last: object = Question.objects.latest("data_uploaded")
-        serializer: object = QuestionSerializer(question_last, many=False)
+        serializer = QuestionSerializer(question_last, many=False)
         return Response({'Последний сохранённый вопрос викторины': serializer.data}, status=status.HTTP_201_CREATED)
+
+    return Response({'Ошибка, повторите'}, status=status.HTTP_400_BAD_REQUEST)
